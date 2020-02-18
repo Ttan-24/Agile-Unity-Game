3df0ae844843891312e293e69410cc57573acd22
Ttan-24 <47601502+Ttan-24@users.noreply.github.com>
2020-02-18 23:39:21 +0000

    Initial Unity Files
    - created initial scene
    - made player with raycasting input
    - made enemies
    - raycasted collision with enemies
    - enemy health
    - enemy movement toward player

---
+++ Assets/LookingScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookingScript : MonoBehaviour
+{
+    public Camera camera;
+    bool mouseClicked;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseClicked = Input.GetMouseButtonDown(0);
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+        if (mouseClicked == true)
+        {
+            if (Physics.Raycast(ray, out hit))
+            {
+                Transform Enemy = hit.transform;
+
+                Enemy.gameObject.GetComponent<HealthEnemy>().health_of_enemy--; 
+                //Destroy(Enemy.gameObject);
+                //Enemy.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+                // Do something with the object that was hit by the raycast.
+            }
+        }
+    }
+}
---
+++ Assets/MovementEnemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementEnemy : MonoBehaviour
+{
+    public GameObject Player;
+    public float moveSpeed;
+   
+    Vector3 Movement = new Vector3();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Movement = Player.gameObject.transform.position - gameObject.transform.position;
+        Movement = Movement.normalized * moveSpeed * Time.deltaTime;
+        gameObject.transform.position += Movement;
+    }
+}

9a21e77469eadedeca5efeef3d2fbd01a8e3a848
ben-glitch <62718600+ben-glitch@users.noreply.github.com>
2020-03-30 20:45:28 +0100

    Basic Key code
    virtural

---
+++ Assets/playerKeys.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerKeys : MonoBehaviour
+{
+    int currentKeys = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        incrementKeys();
+    }
+
+    private void incrementKeys()
+    {
+        currentKeys++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}

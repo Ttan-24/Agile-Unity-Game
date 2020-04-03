988af32d6873b444e1f346753da4d6fe39c5cdea
Tt-24 <47601502+Tt-24@users.noreply.github.com>
2020-04-03 01:03:14 +0100

    Rough draft maze made, Players basic movement for testing done
    Needs to have basic opening doors to understand the maze

--- Assets/MovementPlayer.cs
+++ Assets/MovementPlayer.cs
@@ -4,36 +4,42 @@ using UnityEngine;
 
 public class MovementPlayer : MonoBehaviour
 {
-     private Rigidbody m_rigidbody;
-    public float PlayerSpeed;
+    private Rigidbody m_rigidbody;
+    public float moveSpeed;
+    public float rotationSpeed;
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
         m_rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (Input.GetKey(KeyCode.W))
         {
-            m_rigidbody.velocity += new Vector3(0, 0, PlayerSpeed);
+            m_rigidbody.velocity += transform.right * moveSpeed;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            m_rigidbody.velocity -= new Vector3(0, 0, PlayerSpeed);
+            m_rigidbody.velocity -= transform.right * moveSpeed;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            m_rigidbody.velocity -= new Vector3(PlayerSpeed, 0, 0);
+            m_rigidbody.velocity += transform.forward * moveSpeed;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            m_rigidbody.velocity += new Vector3(PlayerSpeed, 0, 0);
+            m_rigidbody.velocity -= transform.forward * moveSpeed;
         }
-        if (Input.GetKey(KeyCode.Space))
+        float mouseX = Input.GetAxis("Mouse X");
+        transform.Rotate(0, mouseX * rotationSpeed, 0);
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            m_rigidbody.AddForce(new Vector3(0, 50.0f, 0));
+            m_rigidbody.AddForce(new Vector3(0, 1000.0f, 0));
         }
+
+        m_rigidbody.velocity *= 0.93f;
     }
 }

b35d5eb3240b16b193d2aca2a278b7ea73968554
The-Lomax <kryniubest@gmail.com>
2020-04-04 17:11:55 +0100

    new features implementation
    added 2 sprites
    added random patrol movement
    added tracing movement

---
+++ Assets/CameraController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float sensitivity = 1.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yaw += Input.GetAxis("Mouse X");
+        pitch -= Input.GetAxis("Mouse Y");
+
+        transform.localEulerAngles = new Vector3(pitch * sensitivity, yaw * sensitivity, 0.0f);
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/EnemyPatrolScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrolScript : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public Transform[] moveSpots;
+    private int randomSpot;
+    private float waitTime;
+    public float startWaitTime = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomSpot = Random.Range(0, moveSpots.Length - 1);
+        waitTime = startWaitTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
+        {
+            if (waitTime <= 0)
+            {
+                randomSpot = Random.Range(0, moveSpots.Length - 1);
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+}

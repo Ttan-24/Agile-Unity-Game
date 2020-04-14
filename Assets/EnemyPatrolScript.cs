6346e1492e302c0f5b57bbde33519bbfcaa02fd7
The-Lomax <kryniubest@gmail.com>
2020-04-14 21:25:11 +0100

    Updated enemy movement
    i've made all the waypoints and the enemy is patrolling while >20 units away from player, if gets closer starts going towards player, if not then goes back to patrolling

--- Assets/EnemyPatrolScript.cs
+++ Assets/EnemyPatrolScript.cs
@@ -8,30 +8,53 @@ public class EnemyPatrolScript : MonoBehaviour
     public Transform[] moveSpots;
     private int randomSpot;
     private float waitTime;
-    public float startWaitTime = 3.0f;
+    public Transform player;
+    public float startWaitTime = 1.0f;
+    public string walkMode;
 
     // Start is called before the first frame update
     void Start()
     {
         randomSpot = Random.Range(0, moveSpots.Length - 1);
         waitTime = startWaitTime;
+        player = GameObject.FindWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
-
-        if (Vector3.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
+        if (Vector3.Distance(transform.position, player.position) < 20.0f)
         {
-            if (waitTime <= 0)
+            walkMode = "attack";
+        }
+        else
+        {
+            walkMode = "patrol";
+        }
+        if (walkMode == "patrol")
+        {
+            speed = 5.0f;
+            transform.position = Vector3.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+
+            if (Vector3.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
             {
-                randomSpot = Random.Range(0, moveSpots.Length - 1);
-                waitTime = startWaitTime;
+                if (waitTime <= 0)
+                {
+                    randomSpot = Random.Range(0, moveSpots.Length - 1);
+                    waitTime = startWaitTime;
+                }
+                else
+                {
+                    waitTime -= Time.deltaTime;
+                }
             }
-            else
+        }
+        else if (walkMode == "attack")
+        {
+            speed = 30.0f;
+            if (Vector3.Distance(transform.position, player.position) > 5.0f)
             {
-                waitTime -= Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
             }
         }
     }

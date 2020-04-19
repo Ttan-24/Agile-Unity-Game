06a31426562ea52726138de49c415ab1b6fb775c
Xander <62718635+BritBongXander@users.noreply.github.com>
2020-04-19 18:30:01 +0100

    Changed resolution to 1920x1080

--- Assets/SwitchScenes.cs
+++ Assets/SwitchScenes.cs
@@ -5,8 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class SwitchScenes : MonoBehaviour
 {
-    public GameObject EventSystem;
-
     // Start is called before the first frame update
     void Start()
     {

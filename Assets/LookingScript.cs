ba485c80d46f9b449669b1e5e018b7b0427694f3
amgradoska <60396655+amgradoska@users.noreply.github.com>
2020-04-20 19:37:43 +0100

    Revert "Merge branch 'New-Test' of https://github.com/Ttan-24/Agile-Unity-Game into New-Test"
    This reverts commit b83328d1fbad8d15ce6cdd0d2d07bc0e71ff8bdb, reversing
    changes made to ee6da751405270a1732a16996d794b2acff59f94.

--- Assets/LookingScript.cs
+++ Assets/LookingScript.cs
@@ -1,15 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class LookingScript : MonoBehaviour
 {
     public Camera camera;
     bool mouseClicked;
-
-    [Header("Scenes To Load")]
-    public string riddle1;
 
     // Start is called before the first frame update
     void Start()
@@ -43,7 +39,6 @@ public class LookingScript : MonoBehaviour
                 DoorScript doorScript = hitObject.GetComponent<DoorScript>();
                 KeyScript keyScript = hitObject.GetComponent<KeyScript>();
                 HealthEnemy healthScript = hitObject.GetComponent<HealthEnemy>();
-                Riddle1Script riddleScript = hitObject.GetComponent<Riddle1Script>();
 
                 float distance = hit.distance;
                 Debug.Log("Distance from " + hitObject.gameObject.name + ": " + distance);
@@ -60,14 +55,6 @@ public class LookingScript : MonoBehaviour
                     if (healthScript != null)
                     {
                         healthScript.health_of_enemy--;
-                    }
-                    if (riddleScript != null)
-                    {
-                        //open scene
-                        //riddleScript.OpenScene();
-                        Debug.Log("opened");
-                        Screen.lockCursor = false;
-                        SceneManager.LoadScene(riddle1);
                     }
                 }
 

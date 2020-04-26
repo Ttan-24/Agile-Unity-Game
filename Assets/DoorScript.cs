26c0790b3ee44186f3b531b8624ce4ad84e00cbf
ben-glitch <62718600+ben-glitch@users.noreply.github.com>
2020-04-26 12:50:23 +0100

    score and exit condition work

--- Assets/DoorScript.cs
+++ Assets/DoorScript.cs
@@ -30,14 +30,51 @@ public class DoorScript : MonoBehaviour
         {
             if (gameExit)
             {
+               
                 //PlaceHolderForWin
                 string path = Application.dataPath + "/leaderboard.txt";
-                string text = 
-                    timer.gameObject.GetComponent<Text>().text +
-                    ": "+ 
-                    username.gameObject.GetComponent<Text>().text;
+                string read = System.IO.File.ReadAllText(path);
+                int start = 6;
+                int end = 1;
+                string origin = "";
+                origin = System.IO.File.ReadAllText(path);
+                if (origin.Length > 5)
+                {
+                    for (int i = 7; i < read.Length; i++)
+                    {
+                        if (origin[i] == ':')
+                        {
+                            end = i;
+                        }
+                    }
 
-                File.WriteAllText(path, text);
+
+
+                    origin = origin.Remove(end);
+                    origin = origin.Substring(start);
+                    Debug.Log(origin);
+                    Debug.Log(timer.gameObject.GetComponent<Text>().text.Substring(start));
+                    Debug.Log(timer.gameObject.GetComponent<Text>().text.Substring(start).CompareTo(origin));
+
+                    if (timer.gameObject.GetComponent<Text>().text.Substring(start).CompareTo(origin) > 0)
+                    {
+                        string text =
+                        timer.gameObject.GetComponent<Text>().text +
+                        ": " +
+                        username.gameObject.GetComponent<Text>().text;
+                        File.WriteAllText(path, text);
+
+                    }
+                }
+                else
+                {
+                    string text =
+                    timer.gameObject.GetComponent<Text>().text +
+                    ": " +
+                    username.gameObject.GetComponent<Text>().text;
+                    File.WriteAllText(path, text);
+                }
+                
                 SceneManager.LoadScene("WinScene");
             }
             else
